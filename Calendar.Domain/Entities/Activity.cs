d51dc79636aca852a64daaa0f0809616fa483f75
mobach2 <mobach2@gmail.com>
2018-02-09 19:45:43 +0100

    Update to Activity entity

--- Calendar.Domain/Entities/Activity.cs
+++ Calendar.Domain/Entities/Activity.cs
@@ -1,13 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Calendar.Domain.Entities
 {
     class Activity
     {
+        [Required]
         public Guid AcitivityID { get; set; }
+        public string Name { get; set; }
+        public string Note { get; set; }
     }
 }
